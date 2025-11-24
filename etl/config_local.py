b95a00151dbd22b07f9ca40af03f6c84649bbe43
BarshaSaha <36075096+BarshaSaha@users.noreply.github.com>
2025-11-24 15:56:13 +0530

    Update config_local.py

--- etl/config_local.py
+++ etl/config_local.py
@@ -2,3 +2,6 @@ YOUTUBE_API_KEY = "AIzaSyDNMg6GL6H_mWwERLih_UD7AoxDvYRe-Ak"
 
 REDDIT_CLIENT_ID = "<your real ID>"
 REDDIT_CLIENT_SECRET = "<your real secret>"
+# Directory paths
+DATA_DIR = Path("data_raw")
+OUT_DIR = Path("data_out")
